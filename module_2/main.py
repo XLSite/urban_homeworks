17aacc584f47d5ecb6d66e82baef9176669b296a
XLSite <xlsite@yandex.ru>
2024-12-14 12:08:33 +0300

    Upload hw2

---
+++ module_2/main.py
@@ -0,0 +1,5 @@
+num = 3789
+a = print(num // 1000)
+b = print((num % 1000) // 100)
+c = print((num % 100) // 10)
+d = print(num % 10)
---
+++ module_2/module_2_1.py
@@ -0,0 +1,9 @@
+a = 'HELLO'
+b = 'hell'
+a = a[:len(b)]
+ch = 700
+ed = ch % 10
+des = (ch % 100) // 10
+s = ch // 100
+
+print(s, des, ed)
