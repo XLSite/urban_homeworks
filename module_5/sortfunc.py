672f937b08bcc5f062c25ed05f69a60d856aaaa1
ALEXEY SAMOKHIN <135253490+XLSite@users.noreply.github.com>
2025-01-10 10:56:28 +0300

    Add files via upload

---
+++ module_5/sortfunc.py
@@ -0,0 +1,25 @@
+nums = [5, 3, 8, 2, 6, 4]
+
+def bubble_sort(ls):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i+1]:
+                ls[i], ls[i+1] = ls[i+1], ls[i]
+                swapped = True
+
+
+# bubble_sort(nums)
+# print(nums)
+
+def selection_sort(ls):
+    for i in range(len(ls)):
+        lowest = i
+        for j in range(i+1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls[i], ls[lowest] = ls[lowest], ls[i]
+
+selection_sort(nums)
+print(nums)
